56b945c344ddde33e234ce300b70a3ef029554f9
Samir Remtulla <samir.remtulla@gmail.com>
2013-04-15 11:15:21 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FayeTutorial::Application.config.secret_token = '7c3b94a00c9c74f606ebde9333b5cd6c9aee1b69208158e23b6fa8bd4fa5f16986da4e5d840ec214c0dc0d75c80e5494235164daf71b64e4494fc40028113def'

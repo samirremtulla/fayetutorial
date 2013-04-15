b175660541eb950c52a468135e0b2b45226038fe
Samir Remtulla <samir.remtulla@gmail.com>
2013-04-15 11:35:12 -0400

    basic authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 FayeTutorial::Application.routes.draw do
+  get "sessions/new"
+
+  get "sessions/create"
+
+  get '/login' => 'sessions#new', :as => :login
+  post '/login' => 'sessions#create', :as => :login
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
